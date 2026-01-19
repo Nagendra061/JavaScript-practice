e57cc3f492c7d49d07a2886aa35bb963aadb8485
nagendra061 <nagendravarma061@gmail.com>
2026-01-19 12:35:33 +0530

    pushing into nagebranch checking it's work or not.

--- demo.js
+++ demo.js
@@ -1,1 +1,29 @@
-console.log('Hello World')
+// console.log('Hello World')
+// console.log('nagendra varma')
+// this is single line comment
+const name = "nagendra varma"
+console.log(name)
+let age = 22
+console.log(age)
+console.log(`Hi my name is ${name} and i am ${age} years old`)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof 22)
+console.log(typeof 1.5)
+console.log(typeof true)
+console.log(typeof false)
+if (true) {
+    console.log('Hello World')
+}
+
+if (10 > 5) {
+    console.log('Nagendra is greater than 5')
+}
+
+if (10 < 5) {
+    console.log('Nagendra is smaller than 5 or not')
+}
+
+if (10 == 5) {
+    console.log('Nagendra is equal to 5 or not')
+}   
